ec0ef357189cc028fa91ce3ab639721a90c46178
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-10-10 14:42:10 +0500

    !fix: added webpack information

--- config-overrides.js
+++ config-overrides.js
@@ -4,12 +4,5 @@ const rewireAlias = require('react-app-rewire-alias')
 module.exports = function override(config) {
   return rewireAlias.alias({
     components: 'src/components',
-    assets: 'src/assets',
-    styles: 'src/styles',
-    general: 'src/general',
-    context: 'src/context',
-    services: 'src/services',
-    configs: 'src/configs',
-    models: 'src/models',
   })(config)
 }
